2cb8b1a39c655b656a954a620edf2d479d9d5006
Jesús Daniel Medina Cruz <jesus@beek.io>
2023-11-24 14:50:04 -0800

    Content negotiation installed

---
+++ src/main/kotlin/ktorsample/jesusdmedinac/com/data/model/Person.kt
@@ -0,0 +1,6 @@
+package ktorsample.jesusdmedinac.com.data.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Person(val name: String)
--- src/main/kotlin/ktorsample/jesusdmedinac/com/plugins/Routing.kt
+++ src/main/kotlin/ktorsample/jesusdmedinac/com/plugins/Routing.kt
@@ -1,13 +1,20 @@
 package ktorsample.jesusdmedinac.com.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import ktorsample.jesusdmedinac.com.data.model.Person
+import kotlin.random.Random
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/person") {
+            call.respond(Person(Random.nextInt().toString()))
         }
     }
 }
