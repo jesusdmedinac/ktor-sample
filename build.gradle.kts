914eec86ed37909ffbbb1d969a01b7b03177fadc
Jesús Daniel Medina Cruz <jesus@beek.io>
2023-11-30 16:26:48 -0800

    Use app engine plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.google.cloud.tools.gradle.appengine.appyaml.AppEngineAppYamlExtension
 
 val ktor_version: String by project
 val kotlin_version: String by project
@@ -7,6 +8,8 @@ plugins {
     kotlin("jvm") version "1.9.21"
     id("io.ktor.plugin") version "2.3.6"
     kotlin("plugin.serialization") version "1.9.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.google.cloud.tools.appengine") version "2.5.0"
 }
 
 group = "ktorsample.jesusdmedinac.com"
@@ -31,4 +34,14 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+configure<AppEngineAppYamlExtension> {
+    stage {
+        setArtifact("build/libs/${project.name}-all.jar")
+    }
+    deploy {
+        version = "GCLOUD_CONFIG"
+        projectId = "GCLOUD_CONFIG"
+    }
 }
